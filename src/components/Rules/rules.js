61aa056195646b919bae74bb2d8c2a4c3cbeb803
Christopher De La Garza <christopher.salazar73@t-mobile.com>
2021-07-20 12:37:15 -0700

    Added rest of rules

--- src/components/Rules/rules.js
+++ src/components/Rules/rules.js
@@ -58,7 +58,191 @@ const Rules = () => {
                             your best scoring option.
                             <br></br>
                             The score card is divided into an Upper Section and a Lower Section. Scoring combinations for each
-                            section or explained below
+                            section or explained below.
+                            <br></br><br></br>
+                            <table>
+                                <tr>
+                                    <th>Upper Section</th>
+                                    <th>What to Score</th>
+                                </tr>
+                                <tr>
+                                    <td>Aces(Ones)</td>
+                                    <td>Total of Aces only</td>
+                                </tr>
+                                <tr>
+                                    <td>Twos</td>
+                                    <td>Total of Twos only</td>
+                                </tr>
+                                <tr>
+                                    <td>Threes</td>
+                                    <td>Total of Threes only</td>
+                                </tr>
+                                <tr>
+                                    <td>Fours</td>
+                                    <td>Total of Fours only</td>
+                                </tr>
+                                <tr>
+                                    <td>Fives</td>  
+                                    <td>Total of Fives only</td>  
+                                </tr>
+                                <tr>
+                                    <td>Sixes</td>
+                                    <td>Total of Sixes only</td>
+                                </tr>
+                            </table>
+                            <br></br>
+                            To score in the Upper Section, add only the dice with the same number and enter the total in 
+                            the appropriate box. For example, with the dice shown below you could score 9 in the Threes box, 
+                            2 in the Twos box or 4 in the Fours box. 
+                            <br></br><br></br>
+                            Your goal in the Upper Section is to score a total of at least 63 points, to earn a 35-point bonus. 
+                            The bonus points are based on score three of each number (Aces through Sixes); however, you may 
+                            earn the bonus with any combination of scores totaling 63 points or more.
+                            <br></br><br></br>
+                            Each of the Lower Section scoring combinations is explained in the detail in the next column.
+                            <br></br><br></br>
+                            <table>
+                                <tr>
+                                    <th>Lower Section</th>
+                                    <th>What to Score</th>
+                                </tr>
+                                <tr>
+                                    <td>3 of a Kind</td>
+                                    <td>Total of all 5 dice</td>
+                                </tr>
+                                <tr>
+                                    <td>4 of a Kind</td>
+                                    <td>Total of all 5 dice</td>
+                                </tr>
+                                <tr>
+                                    <td>Full House</td>
+                                    <td>25 points</td>
+                                </tr>
+                                <tr>
+                                    <td>Small Straight</td>
+                                    <td>30 points</td>
+                                </tr>
+                                <tr>
+                                    <td>Large Straight</td>
+                                    <td>40 points</td>
+                                </tr>
+                                <tr>
+                                    <td>Yahtzee (5 of a Kind)</td>
+                                    <td>50 points</td>
+                                </tr>
+                                <tr>
+                                    <td>Chance</td>
+                                    <td>Total of all 5 dice</td>
+                                </tr>
+                            </table>
+                            <br></br><br></br>
+                            <b>3 of a Kind:</b> Score in this box only if the dice include 3 or more of the same number. For 
+                            example, with the dice of three showing a 5, one a 2, and the other a 1, you could score 18 points 
+                            in the 3 of a Kind box.
+                            <br></br><br></br>
+                            <i>Other Scoring Options:</i> You could instead score 18 in the <i>Chance</i> box, or you could 
+                            score in the Upper Section: 15 in the Fives box, 2 in the Twos box or 1 in the Aces box.
+                            <br></br><br></br>
+                            <b>4 of a Kind:</b> Score in this box only if the dice include 4 or more of the same number. 
+                            For example, with the dice showing four dice with 2 and the last dice with a 6, you could score 
+                            14 points in the 4 of a Kind box.
+                            <br></br><br></br>
+                            <i>Other Scoring Options:</i> You could instead scofre 14 in the 3 of a Kind box or in the 
+                            <i>Chance</i> box -- or you could score in the Upper Section: 8 in the Twos box, or 6 in the 
+                            Sixes box.
+                            <br></br><br></br>
+                            <b>Full House: </b> Score in this box only if the dice show three of one number and two of another. 
+                            Any Full House is worth 25 points. For example, with the dice showwing three with a 3, and two dice 
+                            with 5, you could score 25 points in the Full House box.
+                            <br></br><br></br>
+                            <i>Other Scoring Options:</i> You could instead score 19 in the 3 of a Kind box or in the <i>Chance</i> 
+                            box -- or you could score in the Upper Section: 9 in the Threes box or 10 in the Five box.
+                            <br></br><br></br>
+                            <b>Small Straight:</b> Score in this box only if the dice show any sequence of four numbers. Any Small 
+                            Straight is worth 30 points. You could score 30 points in the Small Straight box with any of the three 
+                            dice combinations of one dice with 1, second dice with 2, third dice with 3, fourth dice with 4 and the 
+                            fifth dice showing any number.
+                            <br></br>
+                            <b>Or</b> One dice with 2, second dice with 3, third dice with 4, fourth dice with 5, and the last dice 
+                            showing any number.
+                            <br></br>
+                            <b>Or</b> One dice with 3, second dice with 4, third dice with 5, fourth dice with 6, and the last dice 
+                            showing any number.
+                            <br></br><br></br>
+                            <i>Other Scoring Options:</i> You could instead score in the <i>Chance</i> box, or in the appropriate Upper 
+                            Section box.
+                            <br></br><br></br>
+                            <b>Large Straight:</b> Score in this box only if the dice show any sequence of five numbers. Any Large Straight
+                            is worth 40 points.
+                            <br></br>
+                            You could score 40 points in the Large Straight box with either of the two dice combination of 
+                            <br></br>
+                            - One dice with 1, second dice with 2, third dice with 3, fourth dice with 4, and the last dice with 5.
+                            <br></br>
+                            - One dice with 2, second dice with 3, third dice with 4, fourth dice with 5, and the last dice with 6.
+                            <br></br><br></br>
+                            <i>Other Scoring Options: </i> You could instead score in the Small Straight box, the <i>Chance</i> box,
+                            or the appropriate Upper Section box.
+                            <br></br><br></br>
+                            <b>Yahtzee:</b> Score in this box only if the dice show five of the ame number (5 of a kind). A <i>Yahtzee</i> 
+                            example is <br></br>
+                            All dice having a 1. <br></br>
+                            The first <i>Yahtzee</i> you enter in the <i>Yahtzee</i> box is worth 50 points. For each additional <i>Yahtzee</i> 
+                            you roll, you earn a bonus (see <i>Yahtzee Bonus</i>, below).
+                            <br></br><br></br>
+                            <b>Chance:</b> Score the total of any 5 dice in this box. This catch-all category comes in handy when you 
+                            can't (or don't want to) score in another category, and don't want to enter a zero.
+                            <br></br><br></br>
+                            For example, you could score 22 points in the <i>Chance</i> box with the following dice.<br></br>
+                            One dice with a 1, second dice with 4, third dice with 5, fourth and fifth dice with 6s.
+                            <br></br><br></br>
+                            <b>YAHTZEE BONUS:</b> If you roll a <i>Yahtzee</i> and have already filled in the <i>Yahtzee</i> box with 
+                            a 50, you get a 100-point bonus! Take one bonus chip, then place a check mark in the <i>Yahtzee Bonus</i> 
+                            box on your score card. Then fill in one of the 13 boxes on your score card according to the <i>Joker Rules</i>, 
+                            described below. <br></br>
+                            As long as you've score 50 in the <i>Yahtzee</i> box, youget a <i>Yahtzee</i> bonus for each additional <i>Yahtzee</i> 
+                            you roll -- just squeeze in the check marks if necessary.<br></br>
+                            If you roll a <i>Yahtzee</i> and have already entered zero in the <i>Yahtzee</i> box, you do not earn a <i>Yahtzee</i> 
+                            bonus; however, you must fill in one of the 13 boxes on your score card according to the <i>Joker Rules</i> below.
+                            <br></br><br></br>
+                            <b>Joker Rules:</b> Score the total of all 5 dice in the appropriate Upper Section box. If this box has already been 
+                            filled in, score as follows in any open Lower Section box: <br></br>
+                            <i>
+                                3 of a Kind: Total of all 5 dice <br></br>
+                                4 of a Kind: Total of all 5 dice <br></br>
+                                Full House: 25 points <br></br>
+                                Small Straight: 30 points <br></br>
+                                Large Straight: 40 points <br></br>
+                                Chance: Total of all 5 dice <br></br>
+                            </i>
+                            <br></br>
+                            If the appropriate Upper Section box and all Lower Section boxes are filled in, you must enter a zero in any open 
+                            Upper Section box. <br></br>
+                            For example, you roll five 4s. You've already entered zero in the <i>Yahtzee</i> box, and have also scored in the Fours 
+                            box in the Upper Section. The Joker rules allow you to score in any open box in the Lower Section. You decide to score 40 
+                            points in the Large Straight box.
+                            <br></br><br></br>
+                        </p>
+                    <h2>Ending A Game</h2>
+                        <p>
+                            Once each player has filled in all 13 category boxes, the game ends. Each player now adds up his or her 
+                            score as follows: <br></br>
+                            <b>Upper Section:</b> Add up the Upper scores and enter the total in the <i>Total Score</i> box. Enter the 
+                            35-point bonus in the <i>Bonus</i> box if you scored 63 points or more. Then enter the total in the <i>Total</i> 
+                            box. <br></br>
+                            <b>Lower Section:</b> Add up the Lower section scores, and enter the total in the <i>Total</i> of Lower Section box. 
+                            Add 100 points for each check mark in the <i>Yahtzee Bonus</i> boxes (that is, one check mark for each bonus chip).
+                            <br></br><br></br>
+                            <b>Grand Total:</b> Add the Upper Section and Lower Section scores, and enter the total in this box. This is your score 
+                            for the game.
+                        </p>
+                    <h2>How to Win</h2>
+                        <p>
+                            After the scores are tallied, the player with the highest Grand Total wins the game!
+                        </p>
+                    <h2>Solo Play</h2>
+                        <p>
+                            No competition around? Challenge yourself in solitaire play, and try to beat your previous score!
                         </p>
             </div>
 

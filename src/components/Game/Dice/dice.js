e2b161af581f2bdd9f2ce24c6331dc121cd61a5e
Readw1 <readwbrown@gmail.com>
2021-08-07 22:39:57 -0400

    updates for entities

--- src/components/Game/Dice/dice.js
+++ src/components/Game/Dice/dice.js
@@ -7,17 +7,13 @@ import roll4 from './images/roll4.jpg';
 import roll5 from './images/roll5.jpg';
 import roll6 from './images/roll6.jpg';
 
+
 class Dice extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
           dicegroup :{
-            dice0: {
-              name: "dice0",
-              image: roll1,
-              selected: false,
-            },
             dice1: {
               name: "dice1",
               image: roll1,
@@ -38,10 +34,18 @@ class Dice extends Component {
               image: roll1,
               selected: false,
             },
-          }
+            dice5: {
+              name: "dice5",
+              image: roll1,
+              selected: false,
+            },
+          },
+          gamestart:false
 
         }
         this.rollDice = this.rollDice.bind(this);
+        this.startGame = this.startGame.bind(this);
+        this.resetstate = this.resetstate.bind(this);
     }
 
     rollDice(event){
@@ -57,10 +61,28 @@ class Dice extends Component {
         "type": "rolled_dice",
         "data":{
           "player_name": this.props.name,
-          "dice_selected": selectedDice,
+          "dice_to_roll": selectedDice,
         }
       }
       window.client.send(JSON.stringify(diceEvent))
+    }
+
+    startGame(event){
+      this.state.gamestart = true;
+      let startGameEvent = {
+        "timestamp": Date.now(),
+        "type": "game_started",
+        "data":{
+          "player_name": this.props.name,
+        }
+      }
+      console.log("game start button clicked")
+      window.client.send(JSON.stringify(startGameEvent))
+
+    }
+
+    resetstate(event){
+      this.state.gamestart = false;
     }
 
     onIconClick(event) {
@@ -73,6 +95,7 @@ class Dice extends Component {
        }
 
       newState.dicegroup[event.target.id].selected = !this.state.dicegroup[event.target.id].selected;
+      newState.gamestart = this.state.gamestart
       console.log(this.state.dicegroup[event.target.id].name + " selected");
       this.setState({newState,})
     }
@@ -81,35 +104,49 @@ class Dice extends Component {
 
     render() {
         var count = 0;
-        var dicevals = this.props.gameState.data.dice_vals;
-        for (let selection in this.state.dicegroup) {
-          switch(dicevals[count]){
-              case 1:
-                this.state.dicegroup[selection].image = roll1;
-                break;
-              case 2:
-                this.state.dicegroup[selection].image = roll2;
-                break;
-              case 3:
-                this.state.dicegroup[selection].image = roll3;
-                break;
-              case 4:
-                this.state.dicegroup[selection].image = roll4;
-                break;
-              case 5:
-                this.state.dicegroup[selection].image = roll5;
-                break;
-              case 6:
-                this.state.dicegroup[selection].image = roll6;
-                break;
+        this.state.gamestart = this.props.gameState.data.game_started;
+        if (this.props.gameState.data.game_started){
+          var dicevals = this.props.gameState.data.current_turn.last_roll;
+
+          for (let selection in this.state.dicegroup) {
+            switch(dicevals[count].face_value){
+                case 1:
+                  this.state.dicegroup[selection].image = roll1;
+                  break;
+                case 2:
+                  this.state.dicegroup[selection].image = roll2;
+                  break;
+                case 3:
+                  this.state.dicegroup[selection].image = roll3;
+                  break;
+                case 4:
+                  this.state.dicegroup[selection].image = roll4;
+                  break;
+                case 5:
+                  this.state.dicegroup[selection].image = roll5;
+                  break;
+                case 6:
+                  this.state.dicegroup[selection].image = roll6;
+                  break;
+            }
+            count = count + 1;
           }
-          count = count + 1;
         }
+        else{
+          for (let selection in this.state.dicegroup) {
+            this.state.dicegroup[selection].image = roll1;
+          }
+        }
+
 
         return(
 
             <div className="Dice" >
                 <h2>Dice Area</h2>
+                <div>
+                  {!this.state.gamestart && <input type="button" onClick={this.startGame} value="Start Game"></input>}
+                </div>
+                <input type="button" onClick={this.resetstate()} value="reset state"></input>
 
                 {Object.keys(this.state.dicegroup).map(icon => (
                   <div className={this.state.dicegroup.[icon]['selected'] ?

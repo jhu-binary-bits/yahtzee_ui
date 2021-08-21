0be7a13241c1e7aec0e6cd1c49a2c31e3ca3c70e
Read W. Brown <readwbrown@gmail.com>
2021-08-21 13:07:14 -0400

    private chat on the frontend (#19)
    * private chat on the frontend
    
    * yahtzee bonus frontend

--- src/components/Game/Chat/chat.js
+++ src/components/Game/Chat/chat.js
@@ -10,7 +10,9 @@ class Chat extends Component {
         }
         this.handleChange = this.handleChange.bind(this);
         this.submitChat = this.submitChat.bind(this);
-
+        this.renderChat = this.renderChat.bind(this);
+        this.rerender = this.rerender.bind(this);
+        this.renderChatTitle = this.renderChatTitle.bind(this);
     }
 
     handleChange(event) {
@@ -23,17 +25,23 @@ class Chat extends Component {
         var code = event.keyCode || event.which;
         if (code === 13) {
           this.submitChat();
+          this.setState({
+                message: "",
+          });
         }
+
       }
 
       submitChat(event){
+          let dest = document.querySelector('#message_dest').value;
           console.log("The submit chat button was clicked");
           let messageEvent = {
             "timestamp": Date.now(),
             "type": "chat_message",
             "data": {
                 "player_name": this.props.name,
-                "content": this.state.message
+                "content": this.state.message,
+                "destination": dest
             }
           }
           window.client.send(JSON.stringify(messageEvent))
@@ -42,18 +50,52 @@ class Chat extends Component {
           });
       }
 
+    renderChat(){
+      var dest = ""
+      if(document.querySelector('#message_dest') == null){
+        dest = "all"
+      }
+      else {
+        dest = document.querySelector('#message_dest').value;
+      }
+
+      if(dest == "all"){
+        return <textarea id="chatSpace" type="text" value={this.props.gameState.data.chat_transcript}></textarea>
+      }
+      else{
+        let key = this.props.name + "/" + dest
+        return <textarea id="chatSpace" type="text" value={this.props.gameState.data.private_transcripts[key]}></textarea>
+      }
+    }
+
+    rerender(){
+      this.setState({
+          message: this.state.message,
+      })
+    }
+
+    renderChatTitle(){
+      var dest = ""
+      if(document.querySelector('#message_dest') == null){
+        return <div className="ChatTitle" align="center">Chat with: all</div>
+      }
+      else {
+        dest = document.querySelector('#message_dest').value;
+        return <div className="ChatTitle" align="center">Chat with: {dest}</div>
+
+      }
+
+    }
+
 
     render(){
 
         return(
             <div className="Chat">
-
-                <div className="ChatTitle" align="center">
-                    Game Chat
-                </div>
+                {this.renderChatTitle()}
 
                 <div className="ChatSpace">
-                    <textarea id="chatSpace" type="text" value={this.props.gameState.data.chat_transcript}></textarea>
+                    {this.renderChat()}
                 </div>
                 <div>
                 <div className="ChatTyping">
@@ -62,7 +104,16 @@ class Chat extends Component {
 
                 <div >
                     <input type="button" onClick={this.submitChat} value="Submit" className="SubmitChatButton"></input>
+                    <label for="Destination" className="dest">Send to:</label>
+                    <select name="destination" id="message_dest" onChange={this.rerender}>
+                        {Object.entries(this.props.gameState.data.players).map((item, index) => {
+                          if(item[1]!=this.props.name){return <option value={item[1]}>{item[1]}</option>}
+
+                        })}
+                        <option value="all" selected>All</option>
+                    </select>
                 </div>
+
                 </div>
             </div>
         )
